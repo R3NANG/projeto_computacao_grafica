9a4e7a9c9977477846f66c9fb7096e1a4fda8225
Bell Coutinho <bell.sccoutinho@gmail.com>
2021-10-13 19:14:05 -0300

    Adicionando a ellipse

--- src/geometric/Circle.java
+++ src/geometric/Circle.java
@@ -74,4 +74,78 @@ public class Circle {
         }
 
     }
+
+    public static void midPointEllipse(
+            Polygon polygon,
+            float xc, float yc,
+            float rx, float ry) 
+    {
+
+        float dx, dy, d1, d2, x, y;
+        x = 0;
+        y = ry;
+
+        // Initial decision parameter of region 1
+        d1 = (ry * ry) - (rx * rx * ry) + (0.25f * rx * rx);
+        dx = 2 * ry * ry * x;
+        dy = 2 * rx * rx * y;
+
+        // For region 1
+        while (dx < dy)
+        {
+
+            // Print points based on 4-way symmetry
+            polygon.insertPrimitive (Math.round(x + xc), Math.round(y + yc));
+            polygon.insertPrimitive (Math.round(-x + xc), Math.round(y + yc));
+            polygon.insertPrimitive (Math.round(x + xc), Math.round(-y + yc));
+            polygon.insertPrimitive (Math.round(-x + xc),Math.round(-y + yc));
+
+            // Checking and updating value of
+            // decision parameter based on algorithm
+            if (d1 < 0)
+            {
+                x++;
+                dx = dx + (2 * ry * ry);
+                d1 = d1 + dx + (ry * ry);
+            }
+            else
+            {
+                x++;
+                y--;
+                dx = dx + (2 * ry * ry);
+                dy = dy - (2 * rx * rx);
+                d1 = d1 + dx - dy + (ry * ry);
+            }
+        }
+
+        // Decision parameter of region 2
+        d2 = ((ry * ry) * ((x + 0.5f) * (x + 0.5f)))
+            + ((rx * rx) * ((y - 1) * (y - 1)))
+            - (rx * rx * ry * ry);
+
+        // Plotting points of region 2
+        while (y >= 0) {
+
+            // printing points based on 4-way symmetry
+            polygon.insertPrimitive (Math.round(x + xc), Math.round(y + yc));
+            polygon.insertPrimitive (Math.round(-x + xc),Math.round(y + yc));
+            polygon.insertPrimitive (Math.round(x + xc),Math.round(-y + yc));
+            polygon.insertPrimitive (Math.round(-x + xc),Math.round(-y + yc));
+
+            // Checking and updating parameter
+            // value based on algorithm
+            if (d2 > 0) {
+                y--;
+                dy = dy - (2 * rx * rx);
+                d2 = d2 + (rx * rx) - dy;
+            }
+            else {
+                y--;
+                x++;
+                dx = dx + (2 * ry * ry);
+                dy = dy - (2 * rx * rx);
+                d2 = d2 + dx - dy + (rx * rx);
+            }
+        }
+    }
 }
