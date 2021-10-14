542b9343065707edbdf06c90c97e9b6f01299748
Bell Coutinho <bell.sccoutinho@gmail.com>
2021-10-14 12:10:43 -0300

    Adicionado os comentários nas operações

--- src/imageprocessing/operators/Operator.java
+++ src/imageprocessing/operators/Operator.java
@@ -8,10 +8,14 @@ public class Operator {
     public static BufferedImage addition (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+        // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
 
+
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                // Aplica a adição pixel a pixel
                 imageMatrix[i][j] = image1[i][j] + image2[i][j];
             }
         }
@@ -22,8 +26,10 @@ public class Operator {
     public static BufferedImage subtraction (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+         // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
-
+        
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 imageMatrix[i][j] = image1[i][j] - image2[i][j];
@@ -36,10 +42,13 @@ public class Operator {
     public static BufferedImage multiplication (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+        // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
 
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                // Aplica a multiplicação pixel a pixel
                 imageMatrix[i][j] = image1[i][j] * image2[i][j];
             }
         }
@@ -50,10 +59,14 @@ public class Operator {
     public static BufferedImage division (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+
+        // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
 
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                // Aplica a divisão pixel a pixel
                 if (image2[i][j] != 0) {
                     imageMatrix[i][j] = image1[i][j] / image2[i][j];
                 } else {
@@ -68,10 +81,14 @@ public class Operator {
     public static BufferedImage AND (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+        
+         // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
 
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                // Aplica o AND pixel a pixel
                 imageMatrix[i][j] = image1[i][j] & image2[i][j];
             }
         }
@@ -82,10 +99,13 @@ public class Operator {
     public static BufferedImage OR (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+        // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
 
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                // Aplica o OR pixel a pixel
                 imageMatrix[i][j] = image1[i][j] | image2[i][j];
             }
         }
@@ -96,10 +116,13 @@ public class Operator {
     public static BufferedImage XOR (int[][] image1, int[][] image2, boolean doNormalize) {
         int width = image1[0].length;
         int height = image1.length;
+         // Criar uma imagem de Buffer para receber manipulações
         int[][] imageMatrix = new int[height][width];
 
+        // percorre toda imagem pixel a pixel para realizar o efeito
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
+                // Aplica o XOR pixel a pixel
                 imageMatrix[i][j] = image1[i][j] ^ image2[i][j];
             }
         }
